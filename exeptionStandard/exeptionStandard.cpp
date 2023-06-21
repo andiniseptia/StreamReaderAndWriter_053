04b170626cd6262aeaabd8de32d38062bb1e62a9
andiniseptia <andiniseptiandri124@gmail.com>
2023-06-21 11:32:02 +0700

    Menambahkan flag try

---
+++ exeptionStandard/exeptionStandard.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+// Untuk objek exception yang akan digunakan
+#include <array>
+// Untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+	cout << "Awal Program" << endl; // Penanda 1
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		// Pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		// Memanggil array elemen ke 5
+	}
+}
