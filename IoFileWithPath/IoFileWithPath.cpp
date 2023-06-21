684481c3359685f9e393a079d454d65ab50ae60b
andiniseptia <andiniseptiandri124@gmail.com>
2023-06-21 11:01:14 +0700

    Membuka file dalam mode menulis dan menunjuk ke sebuah nama file

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -10,4 +10,11 @@ int main() {
 	cout << "Masukkan Nama File : ";
 	cin >> NamaFile;
 
+	// Membuka file dalam mode menulis 
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file 
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << "> Menulis file, \'q\' untuk keluar" << endl;
+
 }
