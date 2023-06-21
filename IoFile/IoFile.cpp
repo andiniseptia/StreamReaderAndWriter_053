1b589f6aaf99c4302be4031ca36e0c976cd9ef31
andiniseptia <andiniseptiandri124@gmail.com>
2023-06-21 10:35:03 +0700

    Menunjuk ke sebuah nama file

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -8,5 +8,10 @@ int main() {
 
 	// Membuka file dalam mode menulis
 	ofstream outfile;
-	
+	// Menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
 }
