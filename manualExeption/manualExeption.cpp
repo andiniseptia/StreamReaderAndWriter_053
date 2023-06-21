aaaee1bf345467c78fd8c4b1a5c8c81197fdbc3f
andiniseptia <andiniseptiandri124@gmail.com>
2023-06-21 11:24:00 +0700

    Menambahkan flag catch

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -7,4 +7,14 @@ int main() {
 		throw 0.5;  // Melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		// Blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*Jika selain integer maka block ini akan dieksekusi*/
+		cout << "Default pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
 }
